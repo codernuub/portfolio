6c81b5c6c9f6b7c4f0f7b163a3dc3dc752295ea3
codernuub <sardarbhai6@gmail.com>
2020-08-18 16:53:20 +0530

    updte2

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
+/*adjust pddig */
+window.onload = function(){
+  const header = document.querySelector('header');
+  document.querySelector('section').style.paddingTop = header.clientHeight;
+}
+
+
+/*open nv*/
 function openNav() {
     document.querySelector('.menu').classList.toggle('a-menu');
     document.querySelector('nav').classList.toggle('active-nav'); 
 }
+
+/*open know more */
+const works = document.querySelectorAll('#work');
+
+works.forEach(work => {
+    work.addEventListener('click' , ()=>{
+      work.parentElement.children[2].classList.toggle('more');
+    })
+})
+
+
